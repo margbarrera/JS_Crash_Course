4246e6b974065cf0342fb9d5cb8b1944001791bc
margherita <margherita.barrera@gmail.com>
2019-10-19 14:03:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,167 @@
+//////////////////////////////////////////////////////////////////
+// This is the beginning of an app to let people wash their clothes
+// and smell amazing afterwards. Humans can wear their clothes, make
+// them dirty, put them in the laundry basket, check if it's full
+// and eventually wash them all and take them back to their wardrobe
+// when they're clean. Although Laundry is not the most exciting task,
+// in my humble opinion clean clothes are sexy! 
+//////////////////////////////////////////////////////////////////
+
+
+
+//////// THE CLASSES ( CLOTHES, HUMANS, LAUNDRY BASKETS) /////////
+
+class Cloth {
+    constructor(ownerName) {
+        this.ownerName = ownerName
+        this.clean = true
+    }
+
+    isSmelled(human) {
+        if(this.clean) {
+            console.log(`${human}: Sniff sniff...   This smells amazing! It's staying in the wardrobe!`)
+        } else { console.log(`${human}: Sniff sniff... Well, this smells awful. It goes straight into the laundry basket, jesus christ!`)}
+    }
+}
+
+
+class Human {
+    constructor(name, numOfClothesOwned) {
+        this.name = name
+        this.numOfClothesOwned = 0
+        this.wardrobe = []
+        // populating the wardrobe with the number of actual clothes objects that
+        //  was provided as an argument (i wrote the function below as a method)
+            this.addClothes(numOfClothesOwned)
+        
+    }
+
+    putClothesIn(cloth,basket) { 
+        basket.items.push(cloth);
+        this.wardrobe.splice(0,1);
+    }
+
+    checkBasket(basket) {
+        if (basket.isFull()) {
+            console.log(`${this.name}: Time to do laundry! let's empty the ${basket.type} basket!`)
+        } else { console.log(`${this.name}: Still half empty, I can wait a few more days`)}
+    }
+
+    addClothes(num) {
+        this.numOfClothesOwned = this.numOfClothesOwned + num;
+        for(let i=0; i < num; i++) {
+            this.wardrobe.push(new Cloth(this.name))
+        }
+    }
+
+    wearCloth(cloth) {
+        console.log(`${this.name}: I'm using the cloth and I\'m sweating like crazy`)
+        cloth.clean = false;
+    }
+}
+
+class LaundryBasket {
+    constructor(type,maxCapacity) {
+        this.type = type
+        this.maxCapacity = maxCapacity
+        this.items = []
+    }
+
+    isFull() {
+        if(this.items.length == this.maxCapacity) {
+            return true
+        } else { return false}
+    }
+
+    washAndReturn() {
+        // So, in theory this should make every item 'clean' again
+        // and return every item to its owner. It is a little tricky though.
+        console.log(`Starting the washing machine... There are ${this.items.length} clothes in total.`);
+        this.items.forEach(cloth => {
+            // clean it
+            cloth.clean = true;
+
+            // find out who owns the cloth
+            function matchOwnerTagToPerson(person) {
+                if(person.name == cloth.ownerName) {
+                    return true
+                }
+            }
+            let owner = houseMembers.filter(matchOwnerTagToPerson)[0]
+            // And then push it back in their wardrobe
+            owner.wardrobe.push(cloth);
+
+        });
+        this.items = [];
+    }
+}
+//////// THE INSTANCES ARE CREATED: 3 humans, 1 basket, all of the clothes(10)/////////
+
+const margherita = new Human('Margherita',3);
+const pino = new Human('Pino',3);
+const gina = new Human('Gina',4);
+const houseMembers = [margherita, pino, gina];
+
+const whitesBasket = new LaundryBasket('whites',5)
+
+//////// THE INTERACTIONS /////////
+
+// Margherita wears an item of clothing, which makes it no longer clean 
+
+ margherita.wearCloth(margherita.wardrobe[0]);
+
+
+ // Margherita smells the item of clothing, finds it dirty  
+ // and puts it in the laundy basket. Wise woman.
+
+ margherita.wardrobe[0].isSmelled('Margherita');
+ margherita.putClothesIn(margherita.wardrobe[0], whitesBasket);
+
+
+// Gina wears something as well Than she smells all of her clothes,
+// just to be sure, and decides which ones need to be washed.
+// She then proceeds to put them in the basket.
+
+console.log('');
+
+gina.wearCloth(gina.wardrobe[0]);
+
+let i=0
+while ( i < gina.wardrobe.length) {
+    let element = gina.wardrobe[i]
+    element.isSmelled('Gina');
+    if(element.clean) {
+        i++
+    } else { gina.putClothesIn(element,whitesBasket) }
+}
+
+ // Pino wears ALL of his clothes. Mad style!
+ // Afterwards they are all pretty smelly and they get put in the basket too.
+
+ console.log('');
+
+ while (pino.wardrobe.length > 0) {
+     pino.wearCloth(pino.wardrobe[0]);
+     pino.putClothesIn(pino.wardrobe[0], whitesBasket)
+ };
+
+
+// The basket seems full enough, Gina checks:
+
+console.log('');
+
+gina.checkBasket(whitesBasket);
+
+// Turns out her hunch was right: it is full, so she starts the washing machine:
+
+console.log(`pino's wardrobe before the washing has ${pino.wardrobe.length} clean items`)
+
+whitesBasket.washAndReturn();
+
+// Everybody's happy, their clothes are clean and they are successfully back
+// in their owners' wardrobe. The laundry basket is empty again, and that
+// is a wonderful sensation for a laundry basket. Order is restored!
+
+console.log(`pino's wardrobe after the washing has ${pino.wardrobe.length} clean items`)
+console.log(`there are ${whitesBasket.items.length} dirty clothes in the basket. Yay!`);
+
